79ffe17681fb89421933c88d29d952de2b14fe43
Joseph Ilagan <joben.ilagan@gmail.com>
2020-06-28 19:55:19 +0800

    Added newly-created notebooks and data files directly from recording sessions

---
+++ coffeepython.py
@@ -0,0 +1,10 @@
+# regular price
+
+def regular_price(price):
+    return price
+
+
+# senior citizen price
+def sc_price(price):
+    DISCOUNT = .20
+    return price*(1-DISCOUNT)
---
+++ mymodule.py
@@ -0,0 +1,5 @@
+def greeting():
+    return "Hello world."
+
+def second_greeting():
+    return "How are you?"
